a8a45a6bbadaeb6f7d17a407646d5e802e543b4f
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2023-05-23 02:35:39 -0300

    SharedPreference file

--- app/src/main/java/co/imob/version1/view/activity/LoginActivity.java
+++ app/src/main/java/co/imob/version1/view/activity/LoginActivity.java
@@ -1,14 +1,19 @@
 package co.imob.version1.view.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import co.imob.version1.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEtEmail, loginEtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +23,32 @@ public class LoginActivity extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.login_btn_next);
         btn.setOnClickListener(view -> {
             goToSignUpActivity();
+            saveData();
         });
+
+        loginEtEmail = findViewById(R.id.login_et_email);
+        loginEtPassword = findViewById(R.id.login_et_password);
 
     }
 
     public void goToSignUpActivity() {
         Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
         startActivity(intent);
+    }
+
+    public void saveData() {
+        String email = loginEtEmail.getText().toString();
+        String password = loginEtPassword.getText().toString();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("email", email);
+        editor.putString("password", password);
+        editor.apply();
+
+//        https://www.youtube.com/watch?v=fBaeU2sFTvM
     }
 
 }
