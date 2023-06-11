9d16e2e14345949712b007bec284ea92220775f7
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2023-06-11 19:15:57 -0300

    Refactor nbr 500 kk

--- app/src/main/java/co/imob/version1/view/fragment/MoreFragment.java
+++ app/src/main/java/co/imob/version1/view/fragment/MoreFragment.java
@@ -1,7 +1,6 @@
 package co.imob.version1.view.fragment;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,7 +10,6 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import co.imob.version1.R;
-import co.imob.version1.view.activity.LoginActivity;
 
 public class MoreFragment extends Fragment {
 
@@ -20,7 +18,7 @@ public class MoreFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_more, container, false);
     }
 
-//    public void logout() {
+    //    public void logout() {
 //        SharedPreferences sharedPreferences = getSharedPreferences(
 //                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 //        SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -31,6 +29,24 @@ public class MoreFragment extends Fragment {
 //        Intent intent = new Intent(this, LoginActivity.class);
 //        startActivity(intent);
 //        finish(); // Finalizar a atividade atual para que o usuário não possa voltar para a tela anterior usando o botão "Voltar"
+//    }
+
+
+//    public void logout() {
+//        SharedPreferences sharedPreferences = requireContext().getSharedPreferences(
+//                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.remove("email");
+//        editor.remove("password");
+//        editor.apply();
+//
+//        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//        LoginFragment loginFragment = new LoginFragment();
+//        fragmentTransaction.replace(R.id.fragment_container, loginFragment);
+//
+//        fragmentTransaction.commit();
 //    }
 
 }
